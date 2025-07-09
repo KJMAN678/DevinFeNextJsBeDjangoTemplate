fc6a58b1b9e49317ae0ba164ae9a945edc3af850
KJMAN678 <kj.fireman5@gmail.com>
2025-07-09 20:21:58 +0900

    一旦保存.

---
+++ frontend/app/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+
+  const [data, setData] = useState<{test: number }>({test: 0})
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/')
+    .then((res) => res.data)
+    .then((data) => {
+      setData(data)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h2>test</h2>
+      <p>{data.test}</p>
+    </div>
+  )
+}
